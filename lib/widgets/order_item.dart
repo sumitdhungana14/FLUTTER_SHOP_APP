d162749d4d8f374565660f6953ab017c052cc03c
Sumit Dhungana <sumeetdhungana74@gmail.com>
2020-09-16 18:38:09 +0545

    Expanded functionality added in orders screen.

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   const OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool isExpanded = false;
+
+  Widget buildTrailingIconButton(Icon icon, bool val) {
+    return IconButton(
+        onPressed: () {
+          setState(() {
+            isExpanded = val;
+          });
+        },
+        icon: icon);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +29,27 @@ class OrderItem extends StatelessWidget {
       margin: EdgeInsets.all(10),
       child: Column(children: [
         ListTile(
-          title: Text('\$${order.amount}'),
-          trailing: IconButton(onPressed: null, icon: Icon(Icons.expand_more)),
-        )
+          title: Text('\$${widget.order.amount}'),
+          trailing: !isExpanded
+              ? buildTrailingIconButton(Icon(Icons.expand_more), true)
+              : buildTrailingIconButton(Icon(Icons.expand_less), false),
+        ),
+        if (isExpanded)
+          Container(
+            height: 150,
+            child: ListView(
+              children: widget.order.products
+                  .map((prod) => ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          child: Text(prod.quantity.toString()),
+                        ),
+                        title: Text(prod.title),
+                        trailing: Text('\$${prod.price}'),
+                      ))
+                  .toList(),
+            ),
+          ),
       ]),
     );
   }
