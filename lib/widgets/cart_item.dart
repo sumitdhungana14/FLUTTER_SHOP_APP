2c7654d52d5a9e7f739b942cf466b22be1d4e38d
Sumit Dhungana <sumeetdhungana74@gmail.com>
2020-09-17 14:20:46 +0545

    showDialog, Dialog and confirmDismiss on dismissible.

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -9,7 +9,8 @@ class CartItem extends StatelessWidget {
   final double price;
   final int quantity;
 
-  const CartItem({this.id, this.productId, this.title, this.price, this.quantity});
+  const CartItem(
+      {this.id, this.productId, this.title, this.price, this.quantity});
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +19,38 @@ class CartItem extends StatelessWidget {
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
       onDismissed: (_) => cartContainer.removeItem(productId),
+      confirmDismiss: (dismissDirection) => showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: Row(
+                  children: [
+                    Icon(Icons.delete),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text('Delete this item?')
+                  ],
+                ),
+                content: Text(
+                    'Are you sure you want to delete this item from cart?'),
+                actions: [
+                  FlatButton(
+                      onPressed: () => Navigator.of(ctx).pop(false),
+                      child: Text('No')),
+                  FlatButton(
+                      onPressed: () => Navigator.of(ctx).pop(true),
+                      child: Text('Yes')),
+                ],
+              )),
       background: Container(
         padding: EdgeInsets.only(right: 10),
         margin: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
         color: Colors.red,
-        child: Icon(Icons.delete, color: Colors.white, size: 40,),
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
         alignment: Alignment.centerRight,
       ),
       child: Card(
@@ -30,7 +58,8 @@ class CartItem extends StatelessWidget {
           child: Padding(
               padding: EdgeInsets.all(8),
               child: ListTile(
-                leading: CircleAvatar(child: FittedBox(child: Text('\$$price'))),
+                leading:
+                    CircleAvatar(child: FittedBox(child: Text('\$$price'))),
                 title: Text(title),
                 subtitle: Text('Total: \$${price * quantity}'),
                 trailing: Text('$quantity x'),
